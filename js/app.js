66626151ad4616d06242cf5d802dd4176d34d4f5
Jefferson <molinajefferson998@gmail.com>
2025-07-18 02:05:33 +0200

    number

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll(".btn");
-const emergencyNumber = "573001112233"; // Reemplaza con el tuyo
+const emergencyNumber = "573125620680"; // Reemplaza con el tuyo
 
 buttons.forEach((btn) => {
   if (btn.dataset.value) {
